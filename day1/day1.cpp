4ce4b90c9f182a2309a331201a81c2a8616e43cf
tutuzera <arturacb@gmail.com>
2024-01-04 22:09:28 -0300

    solução dia 1, vários pontos para melhorar ...

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,8 +1,71 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+
+    
+string findDigits(const string &line){
+    // find all digits and its locations
+    string digits = "";
+    
+    for(int i = 0; i < line.length(); i++){
+        char c = line[i];
+        if (isdigit(c)) digits += c;
+        else{
+            switch (c){
+                size_t pos;
+                case 'o':
+                    pos = line.find("one", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '1';
+                    break;
+
+                case 'e':
+                    pos = line.find("eight", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '8';
+                    break;
+                
+                case 'n':
+                    pos = line.find("nine", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '9';
+                    break;
+                
+                case 't':
+                    pos = line.find("two", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '2';
+                    pos = line.find("three", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '3';
+                    break;
+            
+                case 'f':
+                    pos = line.find("four", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '4';
+                    pos = line.find("five", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '5';
+                    break;
+            
+                case 's':
+                    pos = line.find("six", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '6';
+                    pos = line.find("seven", i);
+                    if((pos != string::npos) and (pos == i))
+                        digits += '7';
+                    break;
+            }
+        }
+    }
+    return digits;
+}
 
 int main(){
 
@@ -13,10 +76,7 @@ int main(){
     if(input.is_open()){
         while(getline(input, line)){
             cout << line << endl;
-            string digits = "";
-            for(char c: line){
-                if (isdigit(c)) digits += c;
-            }
+            string digits = findDigits(line);
             //cout << digits[0] << " "
             //     << digits[digits.length()-1] << endl;
             char resp[2];
