2fb6e99c7d077d6857d38819870e27a0d09175a9
tutuzera <arturacb@gmail.com>
2024-01-07 19:54:39 -0300

    day2 complete

--- day2/day2.cpp
+++ day2/day2.cpp
@@ -13,11 +13,12 @@ int solve(string line){
     
     int id{};
 
+    int maxRed{}, maxGreen{}, maxBlue{};
+
     line.erase(remove(line.begin(), line.end(), ':'), line.end());
     line.erase(remove(line.begin(), line.end(), ','), line.end());
     line.erase(remove(line.begin(), line.end(), ';'), line.end());
     
-    //cout << line << endl;
     stringstream ss(line);
     deque<string> words;
     string word;
@@ -29,30 +30,24 @@ int solve(string line){
     id = stoi(words[1]);
     words.erase(words.begin(),words.begin()+2);
 
-    //cout << "game with id " << id << endl;
-
     for(int i=0; i< words.size(); i+=2){
-
         int number = stoi(words[i]); 
         string color = words[i+1];
 
         cout << "number: " << number << " color: " << color << endl;
         
-        if (((color == "red") && (number > 12)) ||
-            ((color == "green") && (number > 13)) ||
-            ((color == "blue") && (number > 14))){
-      //      cout << "IMpossible!" << endl;
-            return 0;
-        }
-            
+        if ((color == "red") && (number > maxRed))
+           maxRed = number;
+        else if ((color == "green") && (number > maxGreen))
+            maxGreen = number;
+        else if ((color == "blue") && (number > maxBlue))
+            maxBlue = number;
     }
 
-
-    return id;
+    return maxRed * maxGreen * maxBlue;
 }
 
 int main(){
-    
     ifstream input ("input.txt");
     int ans{};    
     string line{};
